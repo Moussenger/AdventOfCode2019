0bb589982e6c53a3a1eed8b42db5300d96a840de
Mou <dhmourelle@gmail.com>
2022-01-02 02:36:55 +0100

    Allow several inputs for the problems parts

--- src/main/kotlin/edu/mou/adventofcode/tools/DayProblem.kt
+++ src/main/kotlin/edu/mou/adventofcode/tools/DayProblem.kt
@@ -2,8 +2,8 @@ package edu.mou.adventofcode.tools
 
 import java.io.File
 
-abstract class DayProblem<P1, P2>(val day: String, val name: String) {
-    val input: List<String> = File(javaClass.getResource("/day$day/input.txt")!!.path).readLines()
+abstract class DayProblem<P1, P2>(val day: String, val name: String, inputFile: String = "input.txt") {
+    val input: List<String> = File(javaClass.getResource("/day$day/$inputFile")!!.path).readLines()
 
     abstract fun solvePart1(vararg args: String): P1
     abstract fun solvePart2(vararg args: String): P2
--- src/test/kotlin/edu/mou/adventofcode/tools/DayProblemTest.kt
+++ src/test/kotlin/edu/mou/adventofcode/tools/DayProblemTest.kt
@@ -3,6 +3,7 @@ package edu.mou.adventofcode.tools
 import org.junit.jupiter.api.Test
 import kotlin.reflect.KClass
 import kotlin.reflect.full.createInstance
+import kotlin.reflect.full.primaryConstructor
 import kotlin.test.assertEquals
 
 abstract class DayProblemTest<T : DayProblem<P1, P2>, P1, P2>(
@@ -11,14 +12,20 @@ abstract class DayProblemTest<T : DayProblem<P1, P2>, P1, P2>(
     private val expectedPart2: P2,
     private val argsPart1: List<String> = listOf(),
     private val argsPart2: List<String> = listOf(),
+    private val inputPart1: String? = null,
+    private val inputPart2: String? = null
 ) {
     @Test
     fun testPart1() {
-        assertEquals(expectedPart1, dayProblem.createInstance().solvePart1(*argsPart1.toTypedArray()))
+        val constructor = dayProblem.primaryConstructor!!
+        val instance = if (inputPart1 == null) dayProblem.createInstance() else constructor.call(inputPart1)
+        assertEquals(expectedPart1, instance.solvePart1(*argsPart1.toTypedArray()))
     }
 
     @Test
     fun testPart2() {
-        assertEquals(expectedPart2, dayProblem.createInstance().solvePart2(*argsPart2.toTypedArray()))
+        val constructor = dayProblem.primaryConstructor!!
+        val instance = if (inputPart2 == null) dayProblem.createInstance() else constructor.call(inputPart2)
+        assertEquals(expectedPart2, instance.solvePart2(*argsPart2.toTypedArray()))
     }
 }
